70b87c14eb3f4fadb1b9754b31e6cac158528ab4
Evgeniy <evgnec@gmail.com>
2024-05-26 19:58:30 +0300

    upd

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
+const refs = {
+  colorBtn: document.querySelector("button.change-color"),
+  colorText: document.querySelector(".color"),
+  bodyMark: document.querySelector("body"),
+};
+
+refs.colorBtn.addEventListener("click", changeColorBody);
+
+function changeColorBody() {
+  const color = getRandomHexColor();
+  refs.colorText.textContent = color;
+  refs.bodyMark.style.backgroundColor = color;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)

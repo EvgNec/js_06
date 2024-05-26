be5283d2af21ee6c6d1ee87025f4eb69e9fb3fcd
Evgeniy <evgnec@gmail.com>
2024-05-26 19:39:33 +0300

    upd

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,12 +1,25 @@
 const onFormLogin = document.querySelector(".login-form");
 
 onFormLogin.addEventListener("submit", onFormLoginSubmit);
+const elements = {};
 
 function onFormLoginSubmit(e) {
   e.preventDefault();
-  const formData = new FormData(e.currentTarget);
-  formData.forEach((value, name) => {
-    console.log("name", name);
-    console.log("value", value);
-  });
+  elements.email = e.currentTarget.elements.email.value;
+  elements.password = e.currentTarget.elements.password.value;
+  if (elements.email === "") alert("Input login");
+  else if (elements.password === "") alert("Input passwords");
+  else {
+    console.log("elements", elements);
+    resetForm();
+  }
+  //    const formData = new FormData(e.currentTarget);
+  //   formData.forEach((value, name) => {
+  //     console.log("name", name);
+  //     console.log("value", value);
+  //   });
+}
+
+function resetForm() {
+  onFormLogin.reset();
 }

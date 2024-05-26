12ffb09f52fb4dd5244fe4e8c8ab669d848f34f2
Evgeniy <evgnec@gmail.com>
2024-05-26 19:04:17 +0300

    upd

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,12 @@
+const onFormLogin = document.querySelector(".login-form");
 
+onFormLogin.addEventListener("submit", onFormLoginSubmit);
+
+function onFormLoginSubmit(e) {
+  e.preventDefault();
+  const formData = new FormData(e.currentTarget);
+  formData.forEach((value, name) => {
+    console.log("name", name);
+    console.log("value", value);
+  });
+}
